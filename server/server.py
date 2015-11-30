0a7393d2820c16f00d32038a6881e36b7ad86594
Milkey Mouse <milkeymouse@team-ivan.com>
2015-11-29 21:31:38 -0800

    sos

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from aiohttp import web, web_exceptions
 import time
 from collections import defaultdict
 import subprocess
+import asyncssh
 import sys
 
 try:
@@ -13,17 +14,42 @@ except ImportError:
 MAIN_SITE = "http://google.com/"
 TIME_BETWEEN_REQUESTS = 5
 MAX_VM_MEMORY = "256M"
-WAIT_FOR_VM_KILL = 15
-BASE_IMAGE = "cloud-chamber_FINAL_v1.qcow2"
+WAIT_FOR_VM_KILL = 99999 #25
+print("REMEMBER TO REENABLE VM KILLS", file=sys.stderr)
+BASE_IMAGE = "cloud-chamber_FINAL_v2.qcow2"
 VMS_PER_IP = 8
 USE_KVM = True
-DRY_RUN = True
+DRY_RUN = False
 
 vms = set()
 vms_per_ip = defaultdict(set)
 last_requests = defaultdict(int)
 unconnected_vms = set()
 connected_vms = set()
+
+import asyncio, asyncssh, sys
+
+class MySSHClientSession(asyncssh.SSHClientSession):
+    def data_received(self, data, datatype):
+        print(data, end='')
+
+    def connection_lost(self, exc):
+        if exc:
+            print('SSH session error: ' + str(exc), file=sys.stderr)
+
+@asyncio.coroutine
+def run_client():
+    with (yield from asyncssh.connect('localhost')) as conn:
+        chan, session = yield from conn.create_session(MySSHClientSession,
+                                                       'echo $TERM; stty size',
+                                                       term_type='xterm-color',
+                                                       term_size=(80, 24))
+        yield from chan.wait_closed()
+
+try:
+    asyncio.get_event_loop().run_until_complete(run_client())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('SSH connection failed: ' + str(exc))
 
 def save_data():
     with open("server-state.json", "w") as statefile:
@@ -67,8 +93,20 @@ async def start_vm(vm_id):
     if DRY_RUN:
         unconnected_vms.add((time.time(), vm_id, None))
     else:
-        unconnected_vms.add((time.time(), vm_id, subprocess.Popen(["kvm" if USE_KVM else "qemu", "-hda", vm_id + ".qcow2" "-m", MAX_VM_MEMORY])))
+        unconnected_vms.add((time.time(), vm_id, subprocess.Popen(["kvm" if USE_KVM else "qemu", "-hda", vm_id + ".qcow2", "-m", MAX_VM_MEMORY, "-redir", "tcp:" + str(5555+int(vm_id)) + "::22", "-nographic"])))
+    print("Started VM " + vm_id)
     asyncio.ensure_future(kill_unused_vm(vm_id), loop=loop)
+
+async def connect_to_vm(vm_id):
+    to_remove = None
+    for vm in unconnected_vms:
+        if vm[1] == vm_id:
+            connected_vms.add(vm)
+            to_remove = vm
+            break
+    if to_remove:
+        unconnected_vms.remove(to_remove)
+
 
 async def kill_unused_vm(vm_id):
     await asyncio.sleep(WAIT_FOR_VM_KILL)
@@ -130,7 +168,7 @@ async def init(loop):
     app = web.Application(loop=loop, middlewares=[ratelimit_factory])
     app.router.add_route("GET", "/", redirect_to_main)
     app.router.add_route("POST", "/create_vm", create_vm)
-    app.router.add_route("GET", "/vm_start/{vm_id}", start_vm_request)
+    app.router.add_route("GET", "/start_vm/{vm_id}", start_vm_request)
     app.router.add_route("GET", "/ip_vms", vms_for_ip)
 
     srv = await loop.create_server(app.make_handler(), '0.0.0.0', 80)
